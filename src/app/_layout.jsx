94da532ca16c63f61f75eac8ca3a68d0a1020a80
Jimin Lee <jm2147@khu.ac.kr>
2025-08-21 00:43:20 +0900

    [PR] feat: Icon 컴포넌트 구현 (#8)
    * ✨ feat: Icon 컴포넌트 구현

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,34 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import '../../global.css';
+import Icon from '../shared/ui/Icon';
+
 
 export default function App() {
   return (
     <View className="items-center justify-center flex-1 bg-yellowTertiary">
-      <Text className="text-black font-pretendardExtraBold text-title-1">
-        Title1
-      </Text>
-      <Text className="text-grayDark font-pretendardExtraBold text-title-2">
-        Title2
-      </Text>
-      <Text className="text-grayLight font-pretendardSemiBold text-title-3">
-        Title3
-      </Text>
-      <Text className="text-greenPrimary font-pretendardSemiBold text-heading-1">
-        H1
-      </Text>
-      <Text className="text-greenSecondary font-pretendardSemiBold text-heading-2">
-        H2
-      </Text>
-      <Text className="text-greenTertiary font-pretendardMedium text-heading-3">
-        H3
-      </Text>
-      <Text className="text-yellowPrimary font-pretendardMedium text-button-1">
-        Button1
-      </Text>
-      <Text className="text-yellowSecondary font-pretendardMedium text-button-2">
-        Button2
-      </Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around', padding: 20 }}>
+        <Icon name="home" width={30} height={30} />
+        <Icon name="map" width={30} height={30} />
+        <Icon name="journey" width={30} height={30} />
+        <Icon name="heart" width={30} height={30} />
+        <Icon name="profile" width={30} height={30} />
+        <Icon name="gallery" width={30} height={30} />
+      </View>
     </View>
   );
 }
