24bd0827db5350a7426a6cbf3b4da2f187a600e7
Jimin Lee <jm2147@khu.ac.kr>
2025-10-23 20:55:04 +0900

    🔧 chore: clean up (#121)

--- src/store/appearance.store.js
+++ src/store/appearance.store.js
@@ -1,7 +1,7 @@
 import { apiToHeroSlots } from "@utils/appearance.adapters";
 import { create } from "zustand";
 
-const DEFAULT_HERO = { characterId: "base_f", slotBg1: "tent", slotBg23: ["tree"] };
+const DEFAULT_HERO = { characterId: "base_f", slotBg1: "tent", slotBg23: ["tree", "tree"] };
 
 export const useAppearanceStore = create((set) => ({
   heroSlots: DEFAULT_HERO,
